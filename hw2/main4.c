d7774a69a1e990fabf285cea4e2054f80edc7913
b1129042 <68554413+HuangDinXing@users.noreply.github.com>
2024-03-18 22:05:55 +0800

    Update main4.c

--- hw2/main4.c
+++ hw2/main4.c
@@ -1,58 +1,47 @@
-#include <string.h>
 #include <stdio.h>
-typedef struct Books{
+#include <string.h>
+
+typedef struct {
     int id;
     int price;
-    char Title[100];
-}book_t;
+    char title[100];
+} Book;
 
-void total_price(book_t book[],int n){
-    int total=0;
-    for(int i=0;i<n;i++){
-        total+=book[i].price;
+Book bookList[] = {
+    {1, 1000, "All the Light We Cannot See"},
+    {2, 300, "The 38 Letters from J.D. Rockefeller to his son"},
+    {3, 1000, "The Ballad of Songbirds and Snakes"},
+    {4, 550, "Killers of the Flower Moon"},
+    {5, 870, "Elon Musk"},
+    {6, 1344, "Milk and Honey 12-Month 2024"}
+};
+
+void total_price() {
+    int total = 0;
+    for (int i = 0; i < sizeof(bookList) / sizeof(bookList[0]); i++) {
+        total += bookList[i].price;
     }
-    printf("Total price is $%d\n",total);
+    printf("%d\n", total);
 }
-void list_books(book_t book[],int n){
-    for(int i=0;i<n;i++){
-        printf("id=%d Price=%d$ Title=%s\n",book[i].id,book[i].price,book[i].Title);
+
+void list_book(int id) {
+    for (int i = 0; i < sizeof(bookList) / sizeof(bookList[0]); i++) {
+        if (bookList[i].id == id) {
+            printf("%d %s\n", bookList[i].price, bookList[i].title);
+            return;
+        }
     }
-} 
+}
 
+int main() {
+    int N;
+    scanf("%d", &N);
 
+    if (N == 0) {
+        total_price();
+    } else if (N >= 1 && N <= 6) {
+        list_book(N);
+    }
 
-int main()
-{   
-    book_t book[6];
-    
-    book[0].id=1;
-    book[0].price=1000;
-    strcpy(book[0].Title, "All the Light We Cannot See");
-    
-    book[1].id=2;
-    book[1].price=300;
-    strcpy(book[1].Title, "The 38 Letters from J.D. Rockefeller to his son");
-    
-    book[2].id=3;
-    book[2].price=1000;
-    strcpy(book[2].Title, "The Ballad of Songbirds and Snakes");
-    
-    book[3].id=4;
-    book[3].price=550;
-    strcpy(book[3].Title, "Kllers of the Flower Maon");
-    
-    book[4].id=5;
-    book[4].price=870;
-    strcpy(book[4].Title, "Hon Musk");
-    
-    book[5].id=6;
-    book[5].price=1344;
-    strcpy(book[5].Title, "Milk and Honey 12-Month 2024");
-    
-    list_books(book,6);
-    printf("\n");
-    total_price(book,6);
-    
-    
     return 0;
 }
